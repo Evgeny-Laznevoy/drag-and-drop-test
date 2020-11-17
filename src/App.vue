ab50cd30a78b8abbaae0be513c64330cf871133d
Evgeny-Laznevoy <laznevoy86@gmail.com>
2020-11-17 23:55:49 +0500

    drag and drop implementation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div
+      v-for="category in categories"
+      :key="category.id"
+      @drop="onDrop($event, category.id)"
+      class="droppable"
+      @dragover.prevent
+      @dragenter.prevent
+    >
+      <h4>{{ category.title }}</h4>
+      <div
+        v-for="item in items.filter(x => x.categoryId == category.id)"
+        :key="item.id"
+        @dragstart="onDragStart($event, item)"
+        class="draggble"
+        draggable="true"
+      >
+        <h5>{{ item.title }}</h5>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      categories: [
+        {
+          id: 0,
+          title: "Cars",
+        },
+        {
+          id: 1,
+          title: "Animals",
+        },
+      ],
+      items: [
+        {
+          id: 0,
+          title: "Audi",
+          categoryId: 0,
+        },
+        {
+          id: 1,
+          title: "BMW",
+          categoryId: 0,
+        },
+        {
+          id: 2,
+          title: "Cat",
+          categoryId: 1,
+        }
+      ],
+    };
+  },
+  methods: {
+    onDragStart(e, item) {
+      e.dataTransfer.dropEffect = 'move'
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.setData('itemId', String(item.id))
+    },
+    onDrop(e, categoryId) {
+      const itemId = parseInt(e.dataTransfer.getData('itemId'))
+      this.items.map(x => {
+        if (x.id == itemId){
+          x.categoryId = categoryId
+          return x
+        }
+      })
+    }
+  },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .droppable {
+    padding: 15px;
+    border-radius: 5px;
+    background: #2c3e50;
+    margin-bottom: 10px;
+
+    h4 {
+      color: white;
+    }
+  }
+  .draggble {
+    background: white;
+    padding: 5px;
+    border-radius: 5px;
+    margin-bottom: 5px;
+
+    h5 {
+      margin: 0;
+    }
+  }
 }
 </style>
